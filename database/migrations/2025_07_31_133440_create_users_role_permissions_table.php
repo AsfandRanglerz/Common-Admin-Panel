ae2fba7c8ad19175aa42ab5e8a8e2a3474c89e0f
AsfandRanglerz <samiafzaal687@gmail.com>
2025-11-01 00:07:00 -0700

    Updated User role migration

--- database/migrations/2025_07_31_133440_create_users_role_permissions_table.php
+++ database/migrations/2025_07_31_133440_create_users_role_permissions_table.php
@@ -15,7 +15,6 @@ class CreateUsersRolePermissionsTable extends Migration
     {
         Schema::create('users_role_permissions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('role_id')->constrained('roles')->onDelete('cascade');
             $table->foreignId('permission_id')->constrained('permissions')->onDelete('cascade');
             $table->unsignedBigInteger('side_menu_id');
